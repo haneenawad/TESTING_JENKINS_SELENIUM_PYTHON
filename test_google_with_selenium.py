3eede3ddb3caea59a352334df5f90ca479b0baf5
“Haneen <haneen.awad1996@gmail.com>
2022-07-03 13:25:56 +0300

    HEADLESS MODE ADDED

--- test_google_with_selenium.py
+++ test_google_with_selenium.py
@@ -1,7 +1,3 @@
-import time
-import warnings
-from threading import Thread
-
 import pytest
 
 from selenium import webdriver
@@ -10,55 +6,82 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 @pytest.fixture()
 def driver():
+    firefox_driver_binary = "geckodriver.exe"
+    ser_firefox = FirefoxService(firefox_driver_binary)
+    firefox_options = FireFoxOptions()
     chrome_options = ChromeOptions()
-    dc = {
-        "browserName": "chrome",
-        "platformName": "Windows 10"
-    }
 
-    # selenium grid standAlone
-    # chrome_options.add_argument("--headless")  # browser doesnt open when run the test
-    # chrome_options.add_argument("--disable-gpu")  # kartes msa5
-    driver = webdriver.Remote("http://localhost:4444",dc)
+    browser_name = 'firefox'
+    # if isinstance(browserName,list):
+    #     for browser_name in browserName:
+    if browser_name == "firefox-webdriver":
+        driver = webdriver.Firefox(service=ser_firefox)
+    elif browser_name == "firefox":
+        firefox_options.add_argument("--headless")  #with the browser doesnt open
+        dc = {
+            "browserName": "firefox",
+            # "browserVersion": "101.0.1(x64)",
+            "platformName": "Windows 10"
+        }
+        driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc, options=firefox_options)
 
-    #123456
+    elif browser_name == "chrome":
+        # chrome_options.add_argument("--headless")  # browser doesnt open when run the test
+        # chrome_options.add_argument("--disable-gpu")  # kartes msa5
 
+        dc = {
+            "browserName": "chrome",
+            "platformName": "Windows 10"
+        }
+        driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc)
+
+    elif browser_name == "firefox-mobile":
+        firefox_options = FireFoxOptions()
+        firefox_options.add_argument("--width=375")
+        firefox_options.add_argument("--height=812")
+        firefox_options.set_preference("general.useragent.override", "userAgent=Mozilla/5.0 "
+                                                                     "(iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like "
+                                                                     "Gecko) CriOS/101.0.4951.44 Mobile/15E148 Safari/604.1")
+        # firefox_options.set_preference("general.useragent.override", "Nexus 7")
+
+        driver = webdriver.Firefox(service=ser_firefox, options=firefox_options)
+
+    elif browser_name == "android-emulator":
+        dc = {
+            "platformName": "Android",
+            "platformVersion": "8.1.0",
+            "deviceName": "Android Emulator",
+            # "platformVersion": "11.0.0",
+            # "deviceName": "1aaa4ea80404",
+            "automationName": "Appium",
+            # "app": "com.android.chrome",
+            "browserName": "Chrome"
+        }
+        driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
+
+    elif browser_name == "android-phone":
+        dc = {
+            "platformName": "Android",
+            "platformVersion": "11.0.0",
+            "deviceName": "1aaa4ea80404",
+            "automationName": "Appium",
+            "browserName": "Chrome"
+        }
+
+        driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
+    else:
+        raise Exception("driver doesn't exists")
     yield driver
     driver.close()
 
-def test_google_page_title(driver):
-    driver.get('https://www.google.com')
+
+def test_title(driver):
+    driver.get("https://www.google.com/")
     title = driver.title
     driver.save_screenshot("google.png")
-    assert title == str.title("google")
-
-# def test_youtube_page_title(driver):
-#     driver.get('https://www.youtube.com')
-#     title = driver.title
-#     assert title == "Home - YouTube"
-#
-# def test_addition_of_2_and_5_simple(driver):
-#     driver.get('https://www.google.com')
-#     # css_selector, xpath
-#     search_field = driver.find_element(By.NAME, "q")
-#     search_field.click()
-#     search_field.send_keys("2 + 5")
-#     search_field.send_keys(Keys.ENTER)
-#
-#     ans_selector = "#cwos"
-#
-#     # WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,ans_selector)))
-#     # actual_field = driver.find_element(By.CSS_SELECTOR,ans_selector)
-#
-#     actual_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ans_selector)))
-#
-#     assert actual_field.text == str(7)
-
+    assert title == "Google"
